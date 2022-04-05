8fdf2350970271b6cc406e68b4254426851bc90e
Donachie David <stranger@teuton.org>
2022-04-05 10:25:39 +0100

    Change default tab to upload

--- js/tabs.js
+++ js/tabs.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(event) {
     var app = new Vue({
         el: '#tabform',
         data: {
-            tab: 1,
+            tab: 2,
         }
     });
 });
