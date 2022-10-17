2a7a28f4f15c3e06c5b23dc4da9382ad9e313b86
Donachie David <stranger@teuton.org>
2022-10-17 15:14:50 +0100

    Wrap PDF generation in try/catch

--- includes/modes/pdf.php
+++ includes/modes/pdf.php
@@ -1,11 +1,15 @@
 <?php
 
+    ini_set('display_errors',1);
     error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_USER_WARNING & ~ E_NOTICE);
 
     $config = config_mpdf($root,$settings);
+    $config['debug'] = true;
     $mpdf = new \Mpdf\Mpdf($config);
     $mpdf->SetCompression(true);
-    $mpdf->WriteHTML(htmldoc(true,$settings));
-    $mpdf->Output();
+    try {
+        $mpdf->WriteHTML(htmldoc(true,$settings));
+        $mpdf->Output();
+    } catch (Exception $e) { echo "<pre>"; print_r($e); }
 
 ?>
