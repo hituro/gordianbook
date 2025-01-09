20a771d0da26f9234d59fa12096ea46ff65f5f48
Donachie David <stranger@teuton.org>
2025-01-09 22:57:51 +0000

    Raise MPDF version to 8.2

--- includes/modes/pdf.php
+++ includes/modes/pdf.php
@@ -1,15 +1,13 @@
 <?php
-
-    ini_set('display_errors',1);
-    error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_USER_WARNING & ~ E_NOTICE);
-
+    ini_set('display_errors',0);
+    //error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_USER_WARNING & ~E_USER_NOTICE);
+    
     $config = config_mpdf($root,$settings);
-    $config['debug'] = true;
+    $config['debug'] = false;
     $mpdf = new \Mpdf\Mpdf($config);
     $mpdf->SetCompression(true);
     try {
         $mpdf->WriteHTML(htmldoc(true,$settings));
         $mpdf->Output();
     } catch (Exception $e) { echo "<pre>"; print_r($e); }
-
 ?>
